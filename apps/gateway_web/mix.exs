6607d6d7ae2d12b2811a4f9b7a7fc1473b510eb2
Guilherme Canella <guycanella@gmail.com>
2025-10-24 14:26:28 -0300

    update dependencies of gateway_web

--- apps/gateway_web/mix.exs
+++ apps/gateway_web/mix.exs
@@ -64,7 +64,9 @@ defmodule GatewayWeb.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:gateway_core, in_umbrella: true},
+      {:gateway_db, in_umbrella: true}
     ]
   end
 
