2df88a4a57f3556b400ee9a95460408621979161
Guilherme Canella <guycanella@gmail.com>
2025-10-24 14:52:13 -0300

    configure test environment

--- config/test.exs
+++ config/test.exs
@@ -1,18 +1,21 @@
 import Config
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
+config :gateway_db, GatewayDb.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "api_gateway_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
 config :gateway_web, GatewayWebWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "ilGdshAnEqF5jTAK2wJrp+3MmGrKJeV1+QLFYYD2M4B6gcCqxrxraRg6nkR7j1JI",
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warning
 
-# Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
 
-# Enable helpful, but potentially expensive runtime checks
 config :phoenix_live_view,
   enable_expensive_runtime_checks: true
